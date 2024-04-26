c88b1de3b8e125db52a358589b6ddaacde8fd2e6
shu <shu@iqua.co.kr>
2024-04-26 16:13:38 +0900

    update

--- src/components/Login/GoogleLoginButton.jsx
+++ src/components/Login/GoogleLoginButton.jsx
@@ -1,20 +1,20 @@
-import React, {  } from 'react';
-import {
-    Button,
-} from "@chakra-ui/react";
-import { FcGoogle } from 'react-icons/fc'
+// import React, {  } from 'react';
+// import {
+//     Button,
+// } from "@chakra-ui/react";
+// import { FcGoogle } from 'react-icons/fc'
 
 
-function GoogleLoginButton() {
-    function onClick() {
-        // TODO
-        console.log("TODO: google login")
-    }
-    return (
-        <Button disabled={true} bg="white" variant="outline" leftIcon={<FcGoogle />} onClick={onClick}>
-            Login With Google
-        </Button>
-        )
-}
+// function GoogleLoginButton() {
+//     function onClick() {
+//         // TODO
+//         console.log("TODO: google login")
+//     }
+//     return (
+//         <Button disabled={true} bg="white" variant="outline" leftIcon={<FcGoogle />} onClick={onClick}>
+//             Login With Google
+//         </Button>
+//         )
+// }
 
-export default GoogleLoginButton;
+// export default GoogleLoginButton;
--- src/components/Login/TwitterLoginButton.jsx
+++ src/components/Login/TwitterLoginButton.jsx
@@ -1,20 +1,20 @@
-import React, {  } from 'react';
-import {
-    Button,
-} from "@chakra-ui/react";
-import { FaTwitter } from 'react-icons/fa'
+// import React, {  } from 'react';
+// import {
+//     Button,
+// } from "@chakra-ui/react";
+// import { FaTwitter } from 'react-icons/fa'
 
 
-function TwitterLoginButton() {
-    function onClick() {
-        // TODO
-        console.log("TODO: twitter login")
-    }
-    return (
-        <Button disabled={true} colorScheme='twitter' leftIcon={<FaTwitter />} onClick={onClick}>
-            Login With Twitter
-        </Button>
-    )
-}
+// function TwitterLoginButton() {
+//     function onClick() {
+//         // TODO
+//         console.log("TODO: twitter login")
+//     }
+//     return (
+//         <Button disabled={true} colorScheme='twitter' leftIcon={<FaTwitter />} onClick={onClick}>
+//             Login With Twitter
+//         </Button>
+//     )
+// }
 
-export default TwitterLoginButton;
+// export default TwitterLoginButton;
